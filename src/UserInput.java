d45f1c7d35fbf112df0b6b8017f7199e572fd822
nounthanith <nounthanith99@gmail.com>
2025-04-01 10:57:10 +0700

    update loop in game

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,64 +1,62 @@
 import java.util.Scanner;
 
 public class UserInput {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("1 = ខ្លា >> 2 = ឃ្លោក >> 3 = មាន់ >> 4 = បង្កង >> 5 = ក្តាម >> 6 = ត្រី");
-        System.out.print("Pleas input number: ");
+        boolean playAgain = false;
+        do {
+            System.out.println("1 = ខ្លា >> 2 = ឃ្លោក >> 3 = មាន់ >> 4 = បង្កង >> 5 = ក្តាម >> 6 = ត្រី");
+            System.out.print("Please input number: ");
 
-        int num = scanner.nextInt();
+            int num = scanner.nextInt();
 
-        System.out.println("========================");
+            System.out.println("========================");
 
-        if(num > 6) {System.out.println("Number is only 1-6");
+            if (num < 1 || num > 6) {
+                System.out.println("Number is only 1-6");
+                System.out.println("========================");
+                continue; // Skip the rest of the loop and ask again
+            }
 
-        System.out.println("========================");}
+            int dice = (int) (Math.random() * 6) + 1;
 
-        int dice =(int) (Math.random() * 6) + 1;
+            System.out.print("Your prediction is: ");
+            printAnimal(num);
 
-        System.out.print("Your prediction is: " + " ");
-        if(num == 1){
-            System.out.println("ខ្លា");
-        }else if( num == 2){
-            System.out.println("ឃ្លោក");
-        }else if( num == 3){
-            System.out.println("មាន់");
-        }else if( num == 4){
-            System.out.println("បង្កង");
-        }else if( num == 5){
-            System.out.println("ក្តាម");
-        }else if( num == 6){
-            System.out.println("ត្រី");
-        }else if(num > 6){
-            System.out.println("None");
+            System.out.println("========================");
+            System.out.print("Answer: ");
+            printAnimal(dice);
+
+            System.out.println("========================");
+
+            if (num == dice) {
+                System.out.println("You win 😂😂");
+            } else {
+                System.out.println("You lose 😪😪");
+            }
+
+            System.out.println("========================");
+            System.out.print("Do you want to play again? (yes/no): ");
+            scanner.nextLine(); // Consume newline
+            String response = scanner.nextLine().toLowerCase();
+            playAgain = response.equals("y");
+
+        } while (playAgain);
+
+        System.out.println("Thanks for playing! Goodbye!");
+        scanner.close();
+    }
+
+    public static void printAnimal(int num) {
+        switch (num) {
+            case 1 -> System.out.println("ខ្លា");
+            case 2 -> System.out.println("ឃ្លោក");
+            case 3 -> System.out.println("មាន់");
+            case 4 -> System.out.println("បង្កង");
+            case 5 -> System.out.println("ក្តាម");
+            case 6 -> System.out.println("ត្រី");
+            default -> System.out.println("None");
         }
-
-        System.out.println("========================");
-
-        System.out.print("Answer: " );
-
-        if(dice == 1){
-            System.out.println("ខ្លា");
-        }else if( dice == 2){
-            System.out.println("ឃ្លោក");
-        }else if( dice == 3){
-            System.out.println("មាន់");
-        }else if( dice == 4){
-            System.out.println("បង្កង");
-        }else if( dice == 5 ){
-            System.out.println("ក្តាម");
-        }else if( dice == 6){
-            System.out.println("ត្រី");
-        }
-
-        System.out.println("========================");
-
-        if(num == dice){
-            System.out.println("You win😂😂");
-        }else {
-            System.out.println("You lose😪😪");
-        }
-        System.out.println("========================");
     }
 }
