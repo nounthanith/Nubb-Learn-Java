8c0301042d4ef8441151d108fc81c1f145d5a938
nounthanith <nounthanith99@gmail.com>
2025-04-05 06:44:07 +0700

    new date time

---
+++ src/Vorible.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+
+public class Vorible {
+    public static void main(String[] args) {
+
+        // Current Date
+        LocalDate date = LocalDate.now();
+        System.out.println("Current Date: " + date);
+
+        // Current Time
+        LocalTime time = LocalTime.now();
+        System.out.println("Current Time: " + time);
+
+        // Current Date and Time
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.println("Current Date and Time: " + dateTime);
+    }
+}
+
